3ad97a8228142a8f0e64117b18469dfcbf583951
jagarlamudirajesh34 <46401592+jagarlamudirajesh34@users.noreply.github.com>
2021-06-05 08:17:23 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "region" {
+   description = "this is region variable"
+   type = "string"
+}
+
+variable "vpc" {
+   description = "this is vpc variable"
+   type = "string"
+}
+
+variable "vpc_cidr" {
+   description = "vpc cidr block"
+   type = "string"
+}
+
+variable "subnet1" {
+   description = "this is subnet1 variable"
+   type = "string"
+}
+
+variable "subnet1_cidr" {
+   description = "subnet1 cidr description"
+   type = "string"
+}
+
+variable "subnet2" {
+   description = "thi is subnet2 variable"
+   type = "string"
+}
+
+variable "subnet2_cidr" {
+   description = "this is subnet2 cidr"
+   type = "string"
+}
